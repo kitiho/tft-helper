d21e984345de256501d83b2f7053c6d05ab23d1d
enoshima7 <duanjipeng0328@gmail.com>
2022-04-21 11:49:36 +0800

    feat: add final comp

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import championJson from '~/database/champion.json'
-import earlyJson from '~/database/early.json'
+import championJson from '~/assets/database/champion.json'
+import recommendLineup from '~/assets/database/recommend-lineup.json'
+
 type Champion = typeof championJson[0]
 
 const champions = championJson.sort((a, b) => {
   return a.cost - b.cost
 })
+
+const earlyComp = getRecommendEarlyComp(recommendLineup)
+const finalComp = getRecommendFinalComp(recommendLineup)
+
 const cost1Champions = champions.filter(champion => champion.cost === 1)
 const cost2Champions = champions.filter(champion => champion.cost === 2)
 const cost3Champions = champions.filter(champion => champion.cost === 3)
@@ -14,18 +19,42 @@ const cost5Champions = champions.filter(champion => champion.cost === 5)
 
 const filteredChampions = [[...cost1Champions], [...cost2Champions], [...cost3Champions], [...cost4Champions], [...cost5Champions]]
 const currentLineUp = $ref<Champion[]>([])
-const analyzedLineups = $computed(() => findSuitableLineups(currentLineUp, earlyJson))
+const analyzedEarlyLineups = $computed(() => findSuitableLineups(currentLineUp, earlyComp))
+const analyzedFinalLineups = $computed(() => findSuitableLineups(currentLineUp, finalComp))
 
-// 寻找最合适的阵容
-function findSuitableLineups(currentLineup: Champion[], earlyLineup: Record<string, Champion[]>) {
-  const allLineups = Object.keys(earlyLineup).map(key => earlyLineup[key])
+// 获取推荐前期阵容
+function getRecommendEarlyComp(lineup: { name: string; earlyComp: string[]; finalComp: string[] }[]): Record<string, Champion[]> {
+  const earlyComp: any = {}
+  lineup.forEach((v) => {
+    earlyComp[v.name] = v.earlyComp.map((name) => {
+      return champions.find(champion => champion.name.toLowerCase() === name)
+    })
+  })
+  return earlyComp as Record<string, Champion[]>
+}
+
+// 获取推荐最终阵容
+function getRecommendFinalComp(lineup: { name: string; earlyComp: string[]; finalComp: string[] }[]): Record<string, Champion[]> {
+  const finalComp: any = {}
+  lineup.forEach((v) => {
+    finalComp[v.name] = v.finalComp.map((name) => {
+      return champions.find(champion => champion.name.toLowerCase() === name)
+    })
+  })
+
+  return finalComp as Record<string, Champion[]>
+}
+
+// 根据当前阵容寻找最合适的阵容
+function findSuitableLineups(currentLineup: Champion[], recommedLineup: Record<string, Champion[]>) {
+  const allLineups = Object.keys(recommedLineup).map(key => recommedLineup[key])
   const occupyCounts = allLineups.map((earlyLineup) => {
     return getOccupyCount(currentLineup, earlyLineup)
   })
   const suitableSortedLineUps = occupyCounts.map((occupyCount, index) => {
     return {
       occupyCount,
-      lineUp: { name: Object.keys(earlyLineup)[index], value: earlyLineup[Object.keys(earlyLineup)[index]] },
+      lineUp: { name: Object.keys(recommedLineup)[index], value: recommedLineup[Object.keys(recommedLineup)[index]] },
     }
   }).sort((a, b) => {
     return b.occupyCount - a.occupyCount
@@ -46,8 +75,7 @@ function onClickChampion(champion: Champion) {
 }
 
 function getImageSrc(id: string) {
-  const url = new URL(`../assets/images/champions/${id}.png`, import.meta.url).href
-  return url
+  return new URL(`../assets/images/champions/${id}.png`, import.meta.url).href
 }
 
 function isSelected(id: string) {
@@ -72,21 +100,47 @@ function isSelected(id: string) {
     </div>
   </div>
 
-  <div v-for="lineup in analyzedLineups" :key="lineup.lineUp.name">
-    <div font-sans italic text-4xl>
-      {{ lineup.lineUp.name }} {{ lineup.occupyCount }}
+  <div flex="~">
+    <div w-400px>
+      <div v-for="lineup in analyzedEarlyLineups" :key="lineup.lineUp.name">
+        <div font-sans italic text-xl>
+          {{ lineup.lineUp.name }} {{ lineup.occupyCount }}
+        </div>
+        <div w-full flex="~">
+          <div
+            v-for="champion in lineup.lineUp.value" :key="champion.id" w-50px h-50px border border-1 border-gray-500
+            cursor-pointer relative
+          >
+            <img :src="getImageSrc(champion.id)" alt="" w-full h-full>
+            <div
+              v-if="isSelected(champion.id)" absolute top-0 left-0 w-full h-full bg="green-300/50" flex="~"
+              items-center justify-center
+            >
+              <div i-ic-baseline-check text-white text-2xl />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div w-full flex="~">
-      <div
-        v-for="champion in lineup.lineUp.value" :key="champion.id" w-50px h-50px border border-1 border-gray-500
-        cursor-pointer relative
-      >
-        <img :src="getImageSrc(champion.id)" alt="" w-full h-full>
-        <div
-          v-if="isSelected(champion.id)" absolute top-0 left-0 w-full h-full bg="green-300/50" flex="~" items-center
-          justify-center
-        >
-          <div i-ic-baseline-check text-white text-2xl />
+
+    <div w-400px>
+      <div v-for="lineup in analyzedFinalLineups" :key="lineup.lineUp.name">
+        <div font-sans italic text-xl>
+          {{ lineup.lineUp.name }} {{ lineup.occupyCount }}
+        </div>
+        <div w-full flex="~">
+          <div
+            v-for="champion in lineup.lineUp.value" :key="champion.id" w-50px h-50px border border-1 border-gray-500
+            cursor-pointer relative
+          >
+            <img :src="getImageSrc(champion.id)" alt="" w-full h-full>
+            <div
+              v-if="isSelected(champion.id)" absolute top-0 left-0 w-full h-full bg="green-300/50" flex="~"
+              items-center justify-center
+            >
+              <div i-ic-baseline-check text-white text-2xl />
+            </div>
+          </div>
         </div>
       </div>
     </div>
